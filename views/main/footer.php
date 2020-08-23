5fbe845d98e255dd269604d90f505adca3698de4
stefchuu <stefankalenderov@gmail.com>
2020-08-24 00:14:57 +0300

    added useful comments

--- views/main/footer.php
+++ views/main/footer.php
@@ -1,10 +1,13 @@
     </div> <!-- div class main -->
 </div> <!-- main wrapper -->
 
+<!-- JS lightweight libraries includes -->
 <script src="js/libraries/jquery-3.5.1.min.js"></script>
 <script src="js/libraries/rellax.min.js"></script>
 <script src="js/libraries/slick.min.js"></script>
 <script src="js/libraries/intense.min.js"></script>
+
+<!-- JS  files includes -->
 <script src="js/menu.js"></script>
 <script src="js/libraries.js"></script>
 <script src="js/main.js"></script>
